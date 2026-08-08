4a06c4ae267f369351dd25261d0bfa0a8c7c92f9
Nathaniel Adam King <nkin212@wgu.edu>
2026-08-08 04:54:03 -0500

    module import error on e2

--- D417_1/e2_set_vlans_1.py
+++ D417_1/e2_set_vlans_1.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 import os
 import yaml
-from network_connection import EXOSManager
+from network_manager import EXOSManager
 
 env_user = os.environ.get("EXOS_DEFAULT_USER", "admin")
 env_pass = os.environ.get("EXOS_DEFAULT_PASS", "")
