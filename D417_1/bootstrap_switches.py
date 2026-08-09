bc10b2690d6e002be5d95e10b0598a1542149923
Nathaniel Adam King <nkin212@wgu.edu>
2026-08-08 19:19:18 -0500

    updated bootstrap

--- D417_1/bootstrap_switches.py
+++ D417_1/bootstrap_switches.py
@@ -47,11 +47,8 @@ def main():
         try:
             with EXOSManager(sw, username=env_user, password=env_pass) as connection:
 
-                connection.send_cmd("configure account admin password", expect_string=r"New password:")
-                connection.send_cmd(NEW_ADMIN_PASS, expect_string=r"Re-enter password:")
-                connection.send_cmd(NEW_ADMIN_PASS)
-                
-                # Save configuration and handle the (y/N) confirmation prompt
+                # (If current password is blank, pass empty quotes or set inline)
+                connection.send_cmd(f"configure account admin password {NEW_ADMIN_PASS}")
                 connection.send_cmd("save configuration primary", expect_string=r"\(y/N\)")
                 connection.send_cmd("y")
                 print(f"successfully bootstrapped {sw.hostname}")
