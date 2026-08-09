f97b7c01f8a8eec9b896a383e599f453dbf30ffc
Nathaniel Adam King <nkin212@wgu.edu>
2026-08-09 17:06:20 -0500

    a

--- D417_1/_bootstrap_admin.py
+++ D417_1/_bootstrap_admin.py
@@ -46,7 +46,7 @@ def main():
                     print(f"    !! {e}")
 
         except Exception as e:
-            print(f"    !! ERROR Failed to configure {device.hostname}: {e}")
+            print(f"    !! ERROR Failed to configure {sw.hostname}: {e}")
 
 if __name__ == "__main__":
     main()
