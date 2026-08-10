5b2f2f06f2a0ae0d9d6c626c806d4af03e5cd375
Nathaniel Adam King <nkin212@wgu.edu>
2026-08-10 17:14:08 -0500

    a

--- D417_1/network_manager.py
+++ D417_1/network_manager.py
@@ -190,15 +190,16 @@ class EXOSDriver(BaseDriver):
 
     def run_password_rotation(self, manager_instance, account, old_pass, new_pass):
         conn = manager_instance.connection
+        prompt = manager_instance.hostname
         
-        print(f" -- Initiating live operational sequence for '{account}'...")
-        out1 = conn.send_command_timing("configure account admin password")
-        payload_old = f"{old_pass}\n" if old_pass else "\n"
-        out2 = conn.send_command_timing(payload_old)
-        out3 = conn.send_command_timing(f"{new_pass}\n")
-        final_output = conn.send_command_timing(f"{new_pass}\n")
+        print(f"  --  Initiating live operational sequence for '{account}'...")
+        out1 = conn.send_command("configure account admin password", expect_string=r"[:\?]")
+        payload_old = old_pass if old_pass else "\n"
+        out2 = conn.send_command(payload_old, expect_string=r"[:\?]")
+        out3 = conn.send_command(new_pass, expect_string=r"[:\?]")
+        final_output = conn.send_command(new_pass, expect_string=fr"{prompt}")
+        
         return f"{out1}\n{out2}\n{out3}\n{final_output}"
-
 
 
 # EXAMPLE
